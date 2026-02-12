03e2983baac68dfc3549cdc1f56363b57fb67bca
Viktoria <bykava.viktoria@gmail.com>
2026-02-12 13:57:25 +0000

    refactoring: optimisation of the query (reduced redundant join)

--- models/mart/finance_days.sql
+++ models/mart/finance_days.sql
@@ -1,16 +1,14 @@
 WITH total AS (
 SELECT
-     o.date_date
-     ,COUNT(DISTINCT(o.orders_id)) AS total_nb_of_transactions
-     ,ROUND(SUM(s.operational_margin), 0) AS operational_margin
-     ,ROUND(SUM(o.revenue), 0) AS total_revenue
-     ,ROUND(SUM(o.purchase_cost), 0) AS total_purchase_cost
-     ,ROUND(SUM(s.shipping_fee), 0) AS total_shipping_fee
-     ,ROUND(SUM(s.log_cost), 0) AS total_log_cost
-     ,ROUND(SUM(o.quantity), 0) AS total_quantity
- FROM {{ref("int_orders_margin")}} o
- INNER JOIN {{ref("int_orders_operational")}} s
-     USING(date_date)
+     date_date
+     ,COUNT(DISTINCT(orders_id)) AS total_nb_of_transactions
+     ,ROUND(SUM(operational_margin), 0) AS operational_margin
+     ,ROUND(SUM(revenue), 0) AS total_revenue
+     ,ROUND(SUM(purchase_cost), 0) AS total_purchase_cost
+     ,ROUND(SUM(shipping_fee), 0) AS total_shipping_fee
+     ,ROUND(SUM(log_cost), 0) AS total_log_cost
+     ,ROUND(SUM(quantity), 0) AS total_quantity
+ FROM {{ref("int_orders_operational")}} 
 GROUP BY date_date
 )
 SELECT 
